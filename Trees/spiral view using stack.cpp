8deb8ebc3fa913083e0bad47afc8eb4a817e9797
Harsh Dalai <harshnsdalai@gmail.com>
2020-03-27 02:31:26 +0530

    Create spiral view using stack.cpp

---
+++ Trees/spiral view using stack.cpp
@@ -0,0 +1,79 @@
+// C++ Program to find diagonal 
+// sum in a Binary Tree 
+#include <bits/stdc++.h> 
+using namespace std; 
+
+struct Node 
+{ 
+	int data; 
+	struct Node* left; 
+	struct Node* right; 
+}; 
+
+struct Node* newNode(int data) 
+{ 
+	struct Node* Node = (struct Node*)malloc(sizeof(struct Node)); 
+	
+	Node->data = data; 
+	Node->left = Node->right = NULL; 
+
+	return Node; 
+} 
+
+
+void spiralView(Node* root) 
+{ 
+    if (root == NULL) 
+        return; // NULL check 
+  
+    stack<Node*> s1;  
+    stack<Node*> s2;  
+  
+    s1.push(root); 
+
+    while (!s1.empty() || !s2.empty()) { 
+
+        while (!s1.empty()) { 
+            Node* temp = s1.top(); 
+            s1.pop(); 
+            cout << temp->data << " "; 
+  
+
+            if (temp->right) 
+                s2.push(temp->right); 
+            if (temp->left) 
+                s2.push(temp->left); 
+        } 
+  
+
+        while (!s2.empty()) { 
+            Node* temp = s2.top(); 
+            s2.pop(); 
+            cout << temp->data << " "; 
+
+            if (temp->left) 
+                s1.push(temp->left); 
+            if (temp->right) 
+                s1.push(temp->right); 
+        } 
+    } 
+} 
+  
+
+int main() 
+{ 
+	Node* root = newNode(1); 
+    root->left = newNode(2); 
+    root->right = newNode(3); 
+    root->left->left = newNode(7); 
+    root->left->right = newNode(6); 
+    root->right->left = newNode(5); 
+    root->right->right = newNode(4);
+
+	spiralView(root); 
+
+	return 0; 
+} 
+
+// This code is contributed by Aditya Goel 
+
