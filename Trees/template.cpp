ef8730a5ebff0ede9fdb56340f15493e009dd3bd
Harsh Dalai <harshnsdalai@gmail.com>
2020-03-27 02:31:30 +0530

    Update template.cpp

--- Trees/template.cpp
+++ Trees/template.cpp
@@ -1,4 +1,4 @@
-// C++ Program to find diagonal 
+	// C++ Program to find diagonal 
 // sum in a Binary Tree 
 #include <bits/stdc++.h> 
 using namespace std; 
@@ -7,7 +7,8 @@ struct Node
 { 
 	int data; 
 	struct Node* left; 
-	struct Node* right; 
+	struct Node* right;
+	struct Node* nextRight; 
 }; 
 
 struct Node* newNode(int data) 
@@ -20,49 +21,48 @@ struct Node* newNode(int data)
 	return Node; 
 } 
 
-void util(Node* root,  int d, map<int, int> &m)
+
+void connect(Node *p)
 {
-	if (!root)
-	{
-		return;
-	}
+   queue<Node*> q;
+   q.push(p);
+   
+   while(!q.empty())
+   {
+   		Node* curr = q.front();
+   		q.pop();
+   		//cout<<curr->data;
+   		curr->nextRight = q.front();
+   		if (curr->left)
+   		{
 
-	m[d] = m[d] + root->data;
+   			q.push(curr->left);
+   		}
 
+   		if (curr->right)
+   		{
+   			q.push(curr->right);
+   		}
 
-	util(root->left, d+1,m);
-	util(root->right, d, m );
+   		
+   }
 }
 
-
-void diagonalSum(Node* root)
-{
-	int s = 0;
-	map<int, int> m;
-	util(root, 0,m);
-	
-	for(auto x: m){
-		cout<<x.second<<" ";
-	}
-}
 
 int main() 
 { 
-	struct Node* root = newNode(1); 
-	root->left = newNode(2); 
-	root->right = newNode(3); 
-	root->left->left = newNode(9); 
-	root->left->right = newNode(6); 
-	root->right->left = newNode(4); 
-	root->right->right = newNode(5); 
-	root->right->left->right = newNode(7); 
-	root->right->left->left = newNode(12); 
-	root->left->right->left = newNode(11); 
-	root->left->left->right = newNode(10); 
+	Node* root = newNode(1); 
+    root->left = newNode(2); 
+    root->right = newNode(3); 
+    root->left->left = newNode(7); 
+    root->left->right = newNode(6); 
+    root->right->left = newNode(5); 
+    root->right->right = newNode(4);
 
-	diagonalSum(root); 
+	connect(root); 
 
 	return 0; 
 } 
 
 // This code is contributed by Aditya Goel 
+
